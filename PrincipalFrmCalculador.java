5832cc558eafcfe6ed94528ffa1f006aa688c948
pedrokrukz <ppok306@gmail.com>
2024-11-13 15:53:27 -0300

    PrincipalFrmCalculador.java

---
+++ PrincipalFrmCalculador.java
@@ -0,0 +1,13 @@
+package principal;
+
+/**
+ *
+ * @author ppok3
+ */
+class FrmCalculadora {
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
